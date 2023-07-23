805050fe1258333a22aa87ec7f27d7afb1da64c0
Karwan87 <damianuszynski@gmail.com>
2023-07-23 15:20:03 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,17 +3,43 @@ import { useSelector } from 'react-redux';
 import ContactItem from '../ContactItem/ContactItem';
 import styles from './ContactList.module.css';
 
+// const ContactList = () => {
+//   const contacts = useSelector(state => state.contacts);
+//   const filter = useSelector(state => state.filter || '');
+
+//   if (!contacts) {
+//     return <div>No contacts found.</div>;
+//   }
+
+//   const filteredContacts = contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <ul className={styles.ManageResults}>
+//       {filteredContacts.map(contact => (
+//         <ContactItem key={contact.id} contact={contact} />
+//       ))}
+//     </ul>
+//   );
+// };
+// export default ContactList;
+
 const ContactList = () => {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
 
   if (!contacts) {
-    return null;
+    return <div>No contacts found.</div>;
   }
 
   const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
+    contact.name.toLowerCase().includes((filter || '').toLowerCase())
   );
+
+  if (filteredContacts.length === 0) {
+    return <div>No contacts found.</div>;
+  }
 
   return (
     <ul className={styles.ManageResults}>
@@ -23,4 +49,5 @@ const ContactList = () => {
     </ul>
   );
 };
+
 export default ContactList;
