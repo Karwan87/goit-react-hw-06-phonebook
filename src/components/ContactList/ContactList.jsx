0958546a2bcd1f30f78970ea3037bb05f26306af
Karwan87 <damianuszynski@gmail.com>
2023-07-24 21:33:34 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,39 +3,28 @@ import { useSelector } from 'react-redux';
 import ContactItem from '../ContactItem/ContactItem';
 import styles from './ContactList.module.css';
 
-// const ContactList = () => {
-//   const contacts = useSelector(state => state.contacts);
-//   const filter = useSelector(state => state.filter || '');
-
-//   if (!contacts) {
-//     return <div>No contacts found.</div>;
-//   }
-
-//   const filteredContacts = contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-
-//   return (
-//     <ul className={styles.ManageResults}>
-//       {filteredContacts.map(contact => (
-//         <ContactItem key={contact.id} contact={contact} />
-//       ))}
-//     </ul>
-//   );
-// };
-// export default ContactList;
-
-const ContactList = () => {
+const ContactList = ({ onDeleteContact }) => {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
+  console.log(
+    'Redux State:',
+    useSelector(state => state)
+  );
+  console.log('Filtered Contacts:', contacts);
 
-  if (!contacts) {
+  if (!Array.isArray(contacts) || contacts.length === 0) {
     return <div>No contacts found.</div>;
   }
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes((filter || '').toLowerCase())
-  );
+  let filteredContacts = contacts;
+
+  if (filter) {
+    filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+
+  console.log('Filtered Contacts:', filteredContacts);
 
   if (filteredContacts.length === 0) {
     return <div>No contacts found.</div>;
@@ -44,7 +33,11 @@ const ContactList = () => {
   return (
     <ul className={styles.ManageResults}>
       {filteredContacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
+        <ContactItem
+          key={contact.id}
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+        />
       ))}
     </ul>
   );
