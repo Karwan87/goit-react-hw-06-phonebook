f100a2f39574e907e297c2dd6e1fce85ae046c74
Karwan87 <damianuszynski@gmail.com>
2023-07-26 21:54:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContact } from '../redux/contactsSlice';
+import {
+  addContact,
+  deleteContact,
+  setContactsArray,
+} from 'redux/contactsSlice';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
 import { setFilter } from '../redux/filterSlice';
@@ -10,13 +14,13 @@ import styles from './var.module.css';
 
 const App = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts);
+  const contactsArray = useSelector(state => state.contactsArray);
   const filter = useSelector(state => state.filter);
 
   useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
     if (storedContacts) {
-      dispatch(addContact(JSON.parse(storedContacts)));
+      dispatch(setContactsArray(JSON.parse(storedContacts)));
     }
   }, [dispatch]);
 
@@ -32,47 +36,14 @@ const App = () => {
 
   const deleteContactHandler = id => {
     dispatch(deleteContact(id));
+    dispatch(
+      setContactsArray(contactsArray.filter(contact => contact.id !== id))
+    );
   };
 
   const changeFilterHandler = filterValue => {
     dispatch(setFilter(filterValue));
   };
-
-  // const App = () => {
-  //   const dispatch = useDispatch();
-  //   const [contactName, setContactName] = useState('');
-  //   const [contactNumber, setContactNumber] = useState('');
-  //   const contacts = useSelector(state => state.contacts);
-  //   const filter = useSelector(state => state.filter);
-
-  //   useEffect(() => {
-  //     const storedContacts = localStorage.getItem('contacts');
-  //     if (storedContacts) {
-  //       dispatch(addContact(JSON.parse(storedContacts)));
-  //     }
-  //   }, [dispatch]);
-
-  //   const addContactHandler = (name, number) => {
-  //     const newContact = {
-  //       id: nanoid(),
-  //       name: name.trim(),
-  //       number: number.trim(),
-  //     };
-
-  //     dispatch(addContact(newContact));
-  //     setContactName('');
-  //     setContactNumber('');
-  //   };
-
-  //   const deleteContactHandler = id => {
-  //     dispatch(deleteContact(id));
-  //   };
-  //   const changeFilterHandler = filterValue => {
-  //     dispatch(setFilter(filterValue));
-  //   };
-
-  //   console.log('Contacts:', contacts);
-  //   console.log('Filter:', filter);
 
   return (
     <div className={styles.ContactContainer}>
@@ -82,28 +53,12 @@ const App = () => {
       <h2>Contacts</h2>
       <Filter onChangeFilter={changeFilterHandler} />
       <ContactList
-        contacts={contacts}
+        contacts={contactsArray}
         filter={filter}
         onDeleteContact={deleteContactHandler}
       />
     </div>
   );
 };
-
-//   return (
-//     <div className={styles.ContactContainer}>
-//       <h1>Phonebook</h1>
-//       <ContactForm
-//         onAddContact={addContactHandler}
-//         name={contactName}
-//         number={contactNumber}
-//       />
-
-//       <h2>Contacts</h2>
-//       <Filter onChangeFilter={changeFilterHandler} />
-//       <ContactList onDeleteContact={deleteContactHandler} />
-//     </div>
-//   );
-// };
 
 export default App;
